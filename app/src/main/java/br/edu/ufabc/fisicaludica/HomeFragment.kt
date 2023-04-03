cd5588d15ac62b52f5065d15287c101468d40268
Gui <guilherme.g.braz@outlook.com>
2023-04-03 18:42:00 -0300

    começo implementação Home fragment

---
+++ app/src/main/java/br/edu/ufabc/fisicaludica/HomeFragment.kt
@@ -0,0 +1,21 @@
+package br.edu.ufabc.fisicaludica
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.edu.ufabc.fisicaludica.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
