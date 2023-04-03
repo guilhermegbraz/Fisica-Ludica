fe2fbae7abe4878904a11592407e29d3902706f0
Gui <guilherme.g.braz@outlook.com>
2023-04-03 18:41:19 -0300

    main activity com fragmentViewContainer

--- app/src/main/java/br/edu/ufabc/fisicaludica/MainActivity.kt
+++ app/src/main/java/br/edu/ufabc/fisicaludica/MainActivity.kt
@@ -1,13 +1,18 @@
 package br.edu.ufabc.fisicaludica
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ufabc.fisicaludica.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        return
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
     }
 }
