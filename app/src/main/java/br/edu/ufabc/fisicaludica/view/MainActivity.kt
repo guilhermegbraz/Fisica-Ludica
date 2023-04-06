271a31dd77d607a7c8e521d73b2de650578f0f20
Gui <guilherme.g.braz@outlook.com>
2023-04-06 11:02:54 -0300

    implementação inicial da MainViewModel compartilhada

--- app/src/main/java/br/edu/ufabc/fisicaludica/view/MainActivity.kt
+++ app/src/main/java/br/edu/ufabc/fisicaludica/view/MainActivity.kt
@@ -1,18 +1,23 @@
-package br.edu.ufabc.fisicaludica
+package br.edu.ufabc.fisicaludica.view
 
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
 import br.edu.ufabc.fisicaludica.databinding.ActivityMainBinding
+import br.edu.ufabc.fisicaludica.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
     }
 }
