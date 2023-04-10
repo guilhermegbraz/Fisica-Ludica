98fc686682271f6aba8ffca517462139cbd9266b
Gui <guilherme.g.braz@outlook.com>
2023-04-10 00:16:46 -0300

    navegação para tela de resultados

--- app/src/main/java/br/edu/ufabc/fisicaludica/view/fragments/MapGameFragment.kt
+++ app/src/main/java/br/edu/ufabc/fisicaludica/view/fragments/MapGameFragment.kt
@@ -18,7 +18,7 @@ class MapGameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMapGameBinding.inflate(inflater, container, false)
 
         return binding.root
