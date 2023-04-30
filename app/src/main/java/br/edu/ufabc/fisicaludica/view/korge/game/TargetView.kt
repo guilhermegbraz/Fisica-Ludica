5502fc76497b298d77bed8f8dbd91869a6115770
Gui <guilherme.g.braz@outlook.com>
2023-04-30 20:40:04 -0300

    korge view para o alvo

---
+++ app/src/main/java/br/edu/ufabc/fisicaludica/view/korge/game/TargetView.kt
@@ -0,0 +1,29 @@
+package br.edu.ufabc.fisicaludica.view.korge.game
+
+import com.soywiz.korge.view.*
+import com.soywiz.korim.bitmap.Bitmap
+import com.soywiz.korim.color.Colors
+import com.soywiz.korma.geom.Angle
+import org.jbox2d.dynamics.BodyType
+
+fun Container.targetView(width: Double, height: Double, posX: Number, posY: Number,
+                         rotation: Int,targetImageBitmap: Bitmap)
+                = TargetView(width, height, posX, posY, rotation, targetImageBitmap).addTo(this)
+class TargetView(width: Double, height: Double, posX: Number, posY: Number,
+                 rotation: Int, targetImageBitmap: Bitmap): Container() {
+
+    val targetBody = roundRect(width, height, width / 50, width / 50, Colors.TRANSPARENT_WHITE)
+    val type = BodyType.STATIC
+
+    init {
+        targetBody.position(posX.toDouble(), posY.toDouble())
+        //.rotation(Angle.fromDegrees(rotation))
+        val targetImage = image(targetImageBitmap)
+        targetImage.scale(
+            targetBody.width / targetImage.width,
+            targetBody.height / targetImage.height
+        )
+
+        targetBody.addChild(targetImage)
+    }
+}
